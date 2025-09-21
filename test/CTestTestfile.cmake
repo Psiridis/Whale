e63bdc590b4050b9ce59b5c6c7f350023c244c42
Psiridis <psiridis.konstantinos@gmail.com>
2025-09-21 04:56:56 +0300

    Remove redundant cmake files

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Projects/Whale/test
-# Build directory: C:/Projects/Whale/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Projects/Whale/test/unit_tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
